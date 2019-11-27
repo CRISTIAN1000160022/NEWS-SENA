e031de9b2660e06ff90edaf0231a6e28fa9202dd
Angel1555 <cdangel71@misena.edu.co>
2019-11-27 18:19:03 -0500

    DOCS:
    Se adjunta el model de PERSONA

---
+++ Desarrollo/Proyecto Grupal/models/modelPersona.php
@@ -0,0 +1,12 @@
+<?php
+class ModelPersona
+{
+        public $idPersona;
+        public $codTdP;
+        public $priNombre;
+        public $seNombre;
+        public $priApellido;
+        public $seApellido;
+        public $estadoPersona;
+     
+}
