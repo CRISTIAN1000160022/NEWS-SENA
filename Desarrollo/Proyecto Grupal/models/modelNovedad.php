cc077976d5ddbac6385fba0f411839d755c97800
Angel1555 <cdangel71@misena.edu.co>
2019-11-27 18:18:06 -0500

    DOCS:
    Se adjunta el model de NOVEDAD

---
+++ Desarrollo/Proyecto Grupal/models/modelNovedad.php
@@ -0,0 +1,17 @@
+<?php
+class ModelNovedad
+{
+    public $codNovedad;
+    public $idEncargadoN;
+    public $codTdEncargadoN;
+    public $codSedeN;
+    public $codCausaN;
+    public $codTipoNovedad;
+    public $fechaNovedad;
+    public $FormatoRetiro;
+    public $EstadoSofiaPlus;
+    public $idAprendizN;
+    public $codTdAprendizN;
+    public $AprendizFichaN;
+    public $estadoNovedad;
+}
