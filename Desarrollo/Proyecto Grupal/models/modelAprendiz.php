7cd20c5f8879c50b5026ff72b6c280b0a9476cab
Angel1555 <cdangel71@misena.edu.co>
2019-11-27 18:06:50 -0500

    DOCS:
    Se adjunta el model de APRENDIZ

---
+++ Desarrollo/Proyecto Grupal/models/modelAprendiz.php
@@ -0,0 +1,10 @@
+<?php
+    class ModelAprendiz {
+        public $idAprendiz;
+        public $codTD;
+        public $nitEmpresa;
+        public $celular;
+        public $direccion;
+        public $email;
+        public $estadoAprendiz;
+    }
